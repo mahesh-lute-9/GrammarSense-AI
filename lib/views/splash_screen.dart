93aa37b0136f7685fb960c4426a98a92739cb3d5
mahi-codes-dev <maheshlute2005@gmail.com>
2026-06-29 07:05:28 +0530

    added assets dependency into yaml file created simple splash screen and decorated it with the diff. parameters

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -7,6 +5,42 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(15, 15, 16, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 240,
+              width: 240,
+              decoration: BoxDecoration(
+                color: const Color(0xFF16161F),
+                borderRadius: BorderRadius.circular(36),
+                border: Border.all(
+                  color: Colors.white.withOpacity(0.05),
+                  width: 1,
+                ), // Smooth cut corners
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFF6A53E7).withOpacity(0.20),
+                    blurRadius: 25,
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(6),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(28),
+                child: Image.asset(
+                  "assets/images/png/GrammarSense_AI.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
