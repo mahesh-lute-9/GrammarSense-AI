348f1b70b6882c4d18ed174afefd6392902746b5
maheshl-9 <maheshlute2005@gmail.com>
2026-06-30 03:38:41 +0530

    added google fonts package for fonts, added app name and tagline under the logo on splash screen

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -11,31 +12,63 @@ class SplashScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: 240,
-              width: 240,
-              decoration: BoxDecoration(
-                color: const Color(0xFF16161F),
-                borderRadius: BorderRadius.circular(36),
-                border: Border.all(
-                  color: Colors.white.withOpacity(0.05),
-                  width: 1,
-                ), // Smooth cut corners
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color(0xFF6A53E7).withOpacity(0.20),
-                    blurRadius: 25,
-                    spreadRadius: 1,
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Color.fromRGBO(106, 83, 231, 1),
                   ),
-                ],
-              ),
-              padding: const EdgeInsets.all(6),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(28),
-                child: Image.asset(
-                  "assets/images/png/GrammarSense_AI.png",
-                  fit: BoxFit.contain,
+                  child: Image.asset(
+                    "assets/images/png/BagIcon.png",
+                    fit: BoxFit.cover,
+                  ),
                 ),
+
+                Positioned(
+                  right: -15,
+                  top: -20,
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(15, 15, 16, 1),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.asset("assets/images/png/Stars_Image.png"),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 25),
+            Text(
+              "Welcome to",
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 28,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "GrammerSense AI",
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 28,
+                fontWeight: FontWeight.w700,
+                color: Color.fromRGBO(106, 83, 231, 1),
+              ),
+            ),
+            SizedBox(height: 40),
+            Text(
+              "Check Grammer Every Words & Sentences",
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Color.fromRGBO(125, 124, 130, 1),
               ),
             ),
           ],
